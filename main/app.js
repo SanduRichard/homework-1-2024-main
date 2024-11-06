0992646f06c09b81eab96b582d5d77d134030607
SanduRichard <sandurichard22@stud.ase.ro>
2024-11-06 18:29:29 +0200

    Update app.js

--- main/app.js
+++ main/app.js
@@ -1,5 +1,46 @@
-const compress = (a, b = true) => {
-    
+function decomprimare(a, result){
+    for (let i = 0; i < a.length; i++) {
+        let number = parseInt(a[i + 1], 10); 
+        let character = a[i]; 
+
+        result += character.repeat(number);
+
+        i++; 
+    }
+    return result;
 }
 
-module.exports = compress
+const compress = (a, b = true) => {
+
+    let count = 1;
+    let result = ''; 
+
+    if (typeof a !== 'string') {
+        throw new Error("InvalidType");
+    }
+
+    if (b) {
+        for (let i = 0; i < a.length; i++) {
+            let j = i + 1;
+            while (a[i] === a[j]) {
+                count++;
+                if (count === 9) {
+                    j++;
+                    break;
+                } else {
+                    j++;
+                }
+            }
+            result += `${a[i]}${count}`;
+            count = 1; 
+            i = j - 1;
+        }
+        return result;
+    } 
+    else {
+        
+        return decomprimare(a, result);
+    }       
+}
+
+module.exports = compress;
